a8bda01fb964125f5a945c06a5394e99145ddd66
Christian O'Keefe <okeefe.christian94@gmail.com>
2019-05-31 15:45:03 -0700

    updated java and css files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,11 +2,14 @@ var directionsText = document.getElementById("directions-text");
 var userChoiceText = document.getElementById("userchoice-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text")
+var triesText = document.getElementById("tries-left")
 
 computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
+var triesLeft = 10;
 var losses = 0;
+
 directionsText.textContent = "Press any key to guess what the computer is thinking!"
 document.onkeyup = function(event){
     var userGuess = event.key;
@@ -17,11 +20,17 @@ document.onkeyup = function(event){
         wins++;
     } 
     else{
-        losses++;
+        triesLeft--;
+        
+        if(triesLeft === 0){
+            losses++;
+            triesLeft = 10;
+        }
     }
     
     userChoiceText.textContent = "You chose: " + userGuess;
-    winsText.textContent = "wins: " + wins;
-    lossesText.textContent = "losses: " + losses;
+    winsText.textContent = "Wins: " + wins;
+    triesText.textContent = "Tries Left: " + triesLeft;
+    lossesText.textContent = "Losses: " + losses;
    
 };
